7067af88d31cc3a8a993fd2ee8a9d2b08b0534f2
Seva Kumar <seva.seattle@gmail.com>
2022-07-13 20:59:01 -0700

    format the sql better

--- 001_hello_world.sql
+++ 001_hello_world.sql
@@ -1,12 +1,17 @@
 #!/usr/bin/sqlite3
 
+-- --------------------------------------------------
 --SQLite
-select "Hello, World!" || ' Current Sqlite Version: ' || sqlite_version();
+select
+    "Hello, World!" || ' Current Sqlite Version: ' || sqlite_version();
+-- --------------------------------------------------
 
 
+-- --------------------------------------------------
 -- run macos 
 -- $ sqlite3 test.db < 001_hello_world.sql
 
-
+-- -----
 --output
 -->Hello, World! Current Sqlite Version: 3.37.0
+-- --------------------------------------------------
